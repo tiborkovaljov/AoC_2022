6c0902806c3e576ee948023084aeba67a92620f5
tiborkovaljov <tiborkovaljov@gmail.com>
2023-06-07 21:03:54 +0200

    Day 1 COMPLETED

--- Day1/Day1.cpp
+++ Day1/Day1.cpp
@@ -7,9 +7,6 @@
 
 int main()
 {
-    /* PART 1 */
-
-    // 1.
     std::fstream input(R"(C:\Users\Tibor\Desktop\drawer\gss\aoc2022\aoc2022_cpp\Day1\day1.in)");
     std::vector<std::string> info;
     std::string line;
@@ -19,11 +16,9 @@ int main()
 
     input.close();
 
-    // 2.
     int max = 0;
     int elf = 0;
 
-    // 3.
     for(const std::string& item : info) {
         if(item.empty()) {
             elf = 0;
@@ -37,16 +32,11 @@ int main()
         }
     }
 
-    // 4.
     std::cout << max << '\n';
 
 
-    /* PART 2 */
-
-    // 1.
     std::vector<int> elfs2;
 
-    // 2.
     int elf2 = 0;
     for(const std::string& item : info) {
         if(item.empty()) {
@@ -59,11 +49,8 @@ int main()
         elfs2.emplace_back(elf2);
     }
 
-
-    // 3.
     std::sort(elfs2.begin(), elfs2.end());
 
-    // 4.
     int top_three = elfs2[elfs2.size() - 1] + elfs2[elfs2.size() - 2] + elfs2[elfs2.size() - 3];
 
     std::cout << top_three << '\n';
