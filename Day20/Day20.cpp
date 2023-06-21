267091a3b5c4ffcef95066d46add1925d41cdcf6
tiborkovaljov <tiborkovaljov@gmail.com>
2023-06-21 18:01:16 +0200

    Day 20 rework

--- Day20/Day20.cpp
+++ Day20/Day20.cpp
@@ -3,23 +3,27 @@
 #include <fstream>
 
 
-
 int main()
 {
-    std::vector<int> encryptedFile = {1, 2, -3, 3, -2, 0, 4};
+    std::ifstream input("C:\\Users\\Tibor\\Desktop\\drawer\\gss\\AoC\\aoc2022\\aoc2022_cpp\\Day20\\day20.in");
+    std::string line;
+    std::vector<int> data;
 
-    // Perform the mixing operation on the encrypted file
-    for(int i = 0; i < encryptedFile.size(); i++)
+    while(std::getline(input, line))
     {
-        int steps = encryptedFile[i];
+        data.push_back(std::stoi(line));
+    }
+
+    for(int i = 0; i < data.size(); i++)
+    {
+        int steps = data[i];
         int currentIndex = i;
 
-        // Move the number forward or backward in the circular list
         if(steps >= 0)
         {
             while(steps > 0)
             {
-                currentIndex = (currentIndex + 1) % encryptedFile.size();
+                currentIndex = (currentIndex + 1) % data.size();
                 steps--;
             }
         }
@@ -27,27 +31,24 @@ int main()
         {
             while(steps < 0)
             {
-                currentIndex = (currentIndex - 1 + encryptedFile.size()) % encryptedFile.size();
+                currentIndex = (currentIndex - 1 + data.size()) % data.size();
                 steps++;
             }
         }
 
-        // Update the value at the current index in the encrypted file
-        encryptedFile[currentIndex] = encryptedFile[i];
+        data[currentIndex] = data[i];
     }
 
-    // Find the grove coordinates
     int groveCoordinatesSum = 0;
     int currentIndex = 0;
 
     for(int i = 0; i < 3; i++)
     {
-        currentIndex = (currentIndex + 1000) % encryptedFile.size();
-        groveCoordinatesSum += encryptedFile[currentIndex];
+        currentIndex = (currentIndex + 1000) % data.size();
+        groveCoordinatesSum += data[currentIndex];
     }
 
-    // Output the sum of the grove coordinates
-    std::cout << "Sum of the grove coordinates: " << groveCoordinatesSum << '\n';
+    std::cout << groveCoordinatesSum << '\n';
 
     return 0;
 }
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Tibor/Desktop/drawer/gss/aoc2022/aoc2022_cpp
+# Install script for directory: C:/Users/Tibor/Desktop/drawer/gss/AoC/aoc2022/aoc2022_cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Tibor/Desktop/drawer/gss/aoc2022/aoc2022_cpp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Tibor/Desktop/drawer/gss/AoC/aoc2022/aoc2022_cpp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
